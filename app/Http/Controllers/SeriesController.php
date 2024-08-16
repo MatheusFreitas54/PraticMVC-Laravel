51fdf8395b6a400f450353255d130050dc574e6f
Matheus Freitas <matheusrlcm@gmail.com>
2024-08-15 22:21:39 -0300

    Subindo arquivos do projetos

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $series = [
+            'Punisher',
+            'Lost',
+            'Grey\'s Anatomy'
+        ];
+
+        return view('series.index', compact('series'));
+    }
+}
---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout title="Séries">
+
+    <ul>
+        @foreach ($series as $serie)
+            <li>{{$serie}}</li>
+        @endforeach
+    </ul>
+    
+</x-layout>
